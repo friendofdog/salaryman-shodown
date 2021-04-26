a29d0726c55b9ea0f751fb7b35194c108196366d
Kevin Kee <kevinkee9@gmail.com>
2021-04-26 19:49:00 +0900

    WIP classes: Refactor to Typescript
    Temporarily set to 'any', fix this later

---
+++ src/classes.tsx
@@ -0,0 +1,49 @@
+class Salaryman {
+  company: string;
+  cp: number;
+  id: string;
+  name: string;
+  points: number;
+  // stats: {
+  //   conformity: { name: string; val: number };
+  //   loyalty: { name: string; val: number };
+  //   karaoke: { name: string; val: number };
+  //   mucus: { name: string; val: number };
+  //   senority: { name: string; val: number };
+  //   sobriety: { name: string; val: number };
+  // };
+  stats: any;
+  title: string;
+
+  constructor(name?: string, title?: string, company?: string, id?: string) {
+    this.stats = {
+      conformity: { name: "conformity", val: 5 },
+      loyalty: { name: "loyalty", val: 5 },
+      karaoke: { name: "karaoke skill", val: 5 },
+      mucus: { name: "mucus production", val: 5 },
+      senority: { name: "senority", val: 5 },
+      sobriety: { name: "sobriety", val: 5 },
+    };
+    this.name = name || "unnamed";
+    this.title = title || "untitled";
+    this.company = company || "unemployed";
+    this.points = 10;
+    this.cp = 10;
+    this.id = id || "dummy";
+
+    // for (const key in this.stats) {
+    //   Object.defineProperty(this, key, {
+    //     get: () => {
+    //       return this.stats[key];
+    //     },
+    //     set: (value: number) => {
+    //       if (this.stats[key] !== value) {
+    //         this.stats[key] = value;
+    //       }
+    //     },
+    //   });
+    // }
+  }
+}
+
+export { Salaryman };
